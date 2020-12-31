c5845a013ba156d832fddc2db5244b57f13322b9
mcd-50 <ayush.as.shukla@gmail.com>
2020-12-31 19:58:28 +0530

    Fixrs

--- src/service/lead_service.py
+++ src/service/lead_service.py
@@ -5,11 +5,11 @@ class LeadService(base_sdk_service.BaseSDKService):
     def __init__(self, name):
         super().__init__(name)
 
-    def get_by_email(self, key, value):
-        return super().get_by(key, value)
+    def get_by_email(self, value):
+        return super().get_by("email", value)
 
-    def get_by_mobile(self, key, value):
-        return super().get_by(key, value)
+    def get_by_mobile(self, value):
+        return super().get_by("mobile", value)
 
 
 lead_service = LeadService("lead")
