6e93ec7352972f56c96a4844c9946f20c0f734c5
mcd-50 <ayush.as.shukla@gmail.com>
2020-12-31 19:22:59 +0530

    Fixrs

--- src/service/base_sdk_service.py
+++ src/service/base_sdk_service.py
@@ -8,94 +8,98 @@ import wrapper.security_wrapper as security_wrapper
 
 
 class BaseSDKService(object):
-	'''
+    '''
 
-	'''
+    '''
 
-	def __init__(self, name):
-		self.name = name
+    def __init__(self, name):
+        self.name = name
 
-	def create(self, payload, action="create"):
-		apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
+    def create(self, payload, action="create"):
+        apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
 
-		url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
-		).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
-		headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
-		params = {}
-		attributes = payload
+        url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
+        ).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
+        headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
+        params = {}
+        attributes = payload
 
-		if apimapopts.get(action).get("has_authorization") is True:
-			headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
-																		{"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
+        if apimapopts.get(action).get("has_authorization") is True:
+            headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
+                                                                         {"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
 
-		if apimapopts.get(action).get("has_signature") is True:
-			headers.update({"x-signature": security_wrapper.sucurity_wrapper.get_wrapper().process_hmac_signature(
-				value=collection_helper.CollectioHelper.process_serialize_data({"value": attributes}), password=request_wrapper.request_wrapper.get_wrapper().secret)})
+        if apimapopts.get(action).get("has_signature") is True:
+            headers.update({"x-signature": security_wrapper.sucurity_wrapper.get_wrapper().process_hmac_signature(
+                value=collection_helper.CollectioHelper.process_serialize_data({"value": attributes}), password=request_wrapper.request_wrapper.get_wrapper().secret)})
 
-		return request_wrapper.request_wrapper.get_wrapper().process_axios_post(
-			url=url, headers=headers, params=params, data=attributes)
+        return request_wrapper.request_wrapper.get_wrapper().process_axios_post(
+            url=url, headers=headers, params=params, data=attributes)
 
-	def get(self, id=None, action="get"):
-		apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
+    def get(self, id=None, action="get"):
+        apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
 
-		url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
-		).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
-		headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
-		params = {"id": id} if id is not None else dict()
+        url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
+        ).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
+        headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
+        params = {"id": id} if id is not None else dict()
 
-		if apimapopts.get(action).get("has_authorization") is True:
-			headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
-																		{"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
+        if apimapopts.get(action).get("has_authorization") is True:
+            headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
+                                                                         {"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
 
-		return request_wrapper.request_wrapper.get_wrapper().process_axios_get(
-			url=url, headers=headers, params=params)
+        if apimapopts.get(action).get("has_urlencoded") is True:
+            headers.update(
+                {"content-type": "application/x-www-form-urlencoded"})
 
-	def save(self, id, payload, action="save"):
-		apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
+        return request_wrapper.request_wrapper.get_wrapper().process_axios_get(
+            url=url, headers=headers, params=params)
 
-		url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
-		).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
-		headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
-		params = {"id": id}
-		attributes = payload
+    def save(self, id, payload, action="save"):
+        apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
 
-		if apimapopts.get(action).get("has_authorization") is True:
-			headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
-																		{"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
+        url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
+        ).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
+        headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
+        params = {"id": id}
+        attributes = payload
 
-		if apimapopts.get(action).get("has_signature") is True:
-			headers.update({"x-signature": security_wrapper.sucurity_wrapper.get_wrapper().process_hmac_signature(
-				value=collection_helper.CollectioHelper.process_serialize_data({"value": attributes}), password=request_wrapper.request_wrapper.get_wrapper().secret)})
+        if apimapopts.get(action).get("has_authorization") is True:
+            headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
+                                                                         {"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
 
-		return request_wrapper.request_wrapper.get_wrapper().process_axios_put(
-			url=url, headers=headers, params=params, data=attributes)
+        if apimapopts.get(action).get("has_signature") is True:
+            headers.update({"x-signature": security_wrapper.sucurity_wrapper.get_wrapper().process_hmac_signature(
+                value=collection_helper.CollectioHelper.process_serialize_data({"value": attributes}), password=request_wrapper.request_wrapper.get_wrapper().secret)})
 
-	def delete(self, id=None, action="get"):
-		apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
+        return request_wrapper.request_wrapper.get_wrapper().process_axios_put(
+            url=url, headers=headers, params=params, data=attributes)
 
-		url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
-		).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
-		headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
-		params = {"id": id} if id is not None else dict()
+    def delete(self, id=None, action="get"):
+        apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
 
-		if apimapopts.get(action).get("has_authorization") is True:
-			headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
-																		{"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
+        url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
+        ).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
+        headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
+        params = {"id": id} if id is not None else dict()
 
-		return request_wrapper.request_wrapper.get_wrapper().process_axios_delete(
-			url=url, headers=headers, params=params)
+        if apimapopts.get(action).get("has_authorization") is True:
+            headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
+                                                                         {"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
 
-	def _list(self, filter, paging={"page": 1, "limit": 20}, action="list"):
-		apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
+        return request_wrapper.request_wrapper.get_wrapper().process_axios_delete(
+            url=url, headers=headers, params=params)
 
-		url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
-		).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
-		headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
-		params = {**filter, **paging}
+    def _list(self, filter, paging={"page": 1, "limit": 20}, action="list"):
+        apimapopts = constant_helper.ConstantHelper.get_setting_constant().API_MAP.get(self.name)
 
-		if apimapopts.get(action).get("has_authorization") is True:
-			headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
-																		{"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
+        url = collection_helper.CollectioHelper.process_key_join(value=[constant_helper.ConstantHelper.get_setting_constant(
+        ).API_BASE_URL, apimapopts.get(action).prefix, apimapopts.get(action).endpoint], separator="")
+        headers = constant_helper.ConstantHelper.get_setting_constant().API_BASE_HEADER
+        params = {**filter, **paging}
 
-		return request_wrapper.request_wrapper.get_wrapper().process_axios_delete(
-			url=url, headers=headers, params=params)
+        if apimapopts.get(action).get("has_authorization") is True:
+            headers.update({"authorization": "Basic " + base64.b64encode("%(name)s:%(pass)s".encode("utf-8") %
+                                                                         {"name": request_wrapper.request_wrapper.get_wrapper().key, "pass": request_wrapper.request_wrapper.get_wrapper().secret})})
+
+        return request_wrapper.request_wrapper.get_wrapper().process_axios_delete(
+            url=url, headers=headers, params=params)
