1096fc768197a045688f1bb86c2fa54f81ad5a72
mcd-50 <ayush.as.shukla@gmail.com>
2021-03-09 22:43:36 +0530

    Added fetch by customer id

--- joinnector/service/lead_service.py
+++ joinnector/service/lead_service.py
@@ -7,6 +7,9 @@ class LeadService(BaseSDKService):
     def __init__(self, name):
         super().__init__(name)
 
+    def get_by_customer_id(self, customer_id, swap_id=None):
+        return super().get_by("customer_id", customer_id, swap_id)
+
     def get_by_email(self, email, swap_id=None):
         return super().get_by("email", email, swap_id)
 
