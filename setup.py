8e7ac7547638ce09a084667a9c27976f854768d3
mcd-50 <ayush.as.shukla@gmail.com>
2021-03-28 16:19:00 +0530

    Fix

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     license='MIT',
     # Give a short description about your library
     description='Nector python SDK (nector.io is a loyalty and reward APIs platform, that allows businesses to reward deals, offers, wallet points to their customers for various actions.)',
-    author='Ayush Shukla',                   # Type in your name
-    author_email='ayush@nector.io',      # Type in your E-Mail
+    author='Tech, Nector',                   # Type in your name
+    author_email='tech@nector.io',      # Type in your E-Mail
     url='https://github.com/joinnector/rewardpythonsdk',
     # I explain this later on
     download_url='https://github.com/joinnector/rewardpythonsdk/archive/2.5.tar.gz',
