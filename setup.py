fc30fd5662afc1ff270e74fb038ccfa3c1044199
mcd-50 <ayush.as.shukla@gmail.com>
2021-03-29 14:46:02 +0530

    Fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='joinnector',         # How you named your package folder (MyLib)
     packages=find_packages(),
-    version='2.5',      # Start with a small number and increase it with every change you make
+    version='3.0',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
@@ -12,7 +12,7 @@ setup(
     author_email='tech@nector.io',      # Type in your E-Mail
     url='https://github.com/joinnector/rewardpythonsdk',
     # I explain this later on
-    download_url='https://github.com/joinnector/rewardpythonsdk/archive/2.5.tar.gz',
+    download_url='https://github.com/joinnector/rewardpythonsdk/archive/3.0.tar.gz',
     # Keywords that define your package best
     keywords=['nector', 'reward', 'loyalty', 'coins', 'wallets', 'deals'],
     install_requires=[            # I get to this in a second
