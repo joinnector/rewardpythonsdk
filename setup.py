a7b11acc9c939d4ac41754079127dac752c1864f
mcd-50 <ayush.as.shukla@gmail.com>
2021-04-10 16:32:50 +0530

    Updated version and changes in repo

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='joinnector',         # How you named your package folder (MyLib)
     packages=find_packages(),
-    version='3.1',      # Start with a small number and increase it with every change you make
+    version='4.0',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
     description='Nector python SDK (nector.io is a loyalty and reward APIs platform, that allows businesses to reward deals, offers, wallet points to their customers for various actions.)',
     author='Tech, Nector',                   # Type in your name
     author_email='tech@nector.io',      # Type in your E-Mail
-    url='https://github.com/joinnector/rewardpythonsdk',
+    url='https://github.com/joinnector/joinnector-python-sdk',
     # I explain this later on
-    download_url='https://github.com/joinnector/rewardpythonsdk/archive/3.1.tar.gz',
+    download_url='https://github.com/joinnector/joinnector-python-sdk/archive/4.0.tar.gz',
     # Keywords that define your package best
     keywords=['nector', 'reward', 'loyalty', 'coins', 'wallets', 'deals'],
     install_requires=[            # I get to this in a second
